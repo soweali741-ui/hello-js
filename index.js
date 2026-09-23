9cafec8af9eb241aaa85013b90c0082664caa2c1
Ali Sowe <soweali741@gmail.com>
2026-09-23 22:45:39 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("Hello World");
+# Week2-API - Express Backend Deployment
+
+Live Express.js REST API - Built & Deployed
+
+## Tech Stack
+Node.js, Express.js, MySQL, JavaScript, REST API, Git
+
+## Features
+- RESTful API design
+- CRUD operations
+- Express routing
+- Deployed backend
+
+## Deployment
+Build & Deploy My Express API
+
+## Developer
+Ali Sowe - Backend Developer (Node.js/Java)
+SCADA Technician at NAWEC | CCNA Certified
+Open to KL, Malaysia - Employment Pass
+
+Email: soweali741@gmail.com
